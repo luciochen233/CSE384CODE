6ed0d7c6210d0240d8c8abdb158b85e605565475
Lucio Chen <lucio@AppledeMBP.mshome.net>
2019-04-27 17:41:29 -0400

    modify main

--- hw5/nopclient.c
+++ hw5/nopclient.c
@@ -1,18 +1,21 @@
 #include "socket_helper.h"
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char **argv) 
+#include <unistd.h>
+int main(int argc, char **argv)
 {
     int clientfd, port;
     char *host, buf[MAXLINE];
-    if (argc != 3) 
+    if (argc != 3)
     {
-    fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
-    exit(0);
+        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+        exit(0);
     }
     host = argv[1];
     port = atoi(argv[2]);
     clientfd = open_clientfd(host, port);
+    int counter = write(clientfd,"lucio",6);
+    printf("\n%d\n",counter);
     //TODO Your homework code goes below
     exit(0);
 }
--- hw5/nopserver.c
+++ hw5/nopserver.c
@@ -1,25 +1,37 @@
 #include "socket_helper.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 int main(int argc, char **argv)
 {
     int listenfd, connfd, port;
     socklen_t clientlen;
     struct sockaddr_in clientaddr;
-    struct hostent *hp; char *haddrp;
-    if (argc != 2) 
+    struct hostent *hp;
+    char *haddrp;
+    if (argc != 2)
     {
         fprintf(stderr, "usage: %s <port>\n", argv[0]);
         exit(0);
     }
     port = atoi(argv[1]);
     listenfd = open_listenfd(port);
-    while (1) 
+    while (1)
     {
         clientlen = sizeof(clientaddr);
         connfd = accept(listenfd, (SA *)&clientaddr, &clientlen);
         printf("Socket connected: %d\n", connfd);
+        char buf[100];
+        int len = 0;
+        while ((len = read(connfd, buf, 100)) <= 0) // read until reach the end
+        {
+            if(len <0) printf("error");
+            write(1, buf, len);
+            //printf("read\n");
+        }
+        printf("\n%s\n",buf);
+        
         //TODO Your homework code goes below
-    } 
+    }
     exit(0);
 }
